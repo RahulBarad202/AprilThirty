515245a4242baa164c0a800c4b67f3a354321422
RahulBarad202 <rahulbarad202@gmail.com>
2025-03-30 19:06:07 +0530

    Update Rambo.java

--- src/ABC/Rambo.java
+++ src/ABC/Rambo.java
@@ -4,7 +4,7 @@ public class Rambo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi is added in github repo ):
+		System.out.println("Hi is added in github repo"):
 			
 
 	}
